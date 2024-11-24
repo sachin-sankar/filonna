51851dd4c29bff9597c02e4b0b669c44892221bc
sachin-sankar <mail.sachinsankar@gmail.com>
2024-11-24 23:49:35 +0530

    feat(config): Command to create filonna.ini

--- src/filonna/__init__.py
+++ src/filonna/__init__.py
@@ -1,10 +1,25 @@
 import click
+from filonna.helpers import get_config_file_path
+from rich import print
 
 
-@click.command()
-def hello():
-    click.echo("Hello World!")
+@click.group()
+def cli():
+    pass
+
+
+@cli.group("config")
+def config():
+    """Work wih filonna config"""
+    pass
+
+
+@config.command()
+def init():
+    """Create filonna.ini in your OS respective config folder"""
+    path = get_config_file_path()
+    print(f"[green]Created filonna.ini at [/][blue bold]{path}[/]")
 
 
 def main():
-    hello()
+    cli()
